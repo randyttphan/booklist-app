d5dea66fb18638b8b62439f3a6d9254b42121bd8
randyttphane <randyttphan@gmail.com>
2019-09-30 23:48:38 -0500

    populated UI with stored books

--- app.js
+++ app.js
@@ -1,13 +1,53 @@
 //Book Class represents a book
 class Book {
-  constructor(title, author, isbn) {}
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
 }
 
 //UI Class to handle UI tasks
+class UI {
+  static displayBooks() {
+    const StoredBooks = [
+      {
+        title: "Book One",
+        author: "John Doe",
+        isbn: "343434"
+      },
+      {
+        title: "Book Two",
+        author: "Jane Doe",
+        isbn: "45545"
+      }
+    ];
+
+    const books = StoredBooks;
+
+    books.forEach(book => UI.addBookToList(book));
+  }
+
+  static addBookToList(book) {
+    const list = document.querySelector("#book-list");
+
+    const row = document.createElement("tr");
+
+    row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+        `;
+
+    list.appendChild(row);
+  }
+}
 
 //Store Class handles local storage
 
 //Event to display books
+document.addEventListener("DOMContentLoaded", UI.displayBooks);
 
 //Event to add a book
 
