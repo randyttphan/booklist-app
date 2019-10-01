8444a8466aed02eb07d3578cb3640e09424ef2ea
randyttphane <randyttphan@gmail.com>
2019-09-30 23:37:59 -0500

    initial commit, uploading HTML

---
+++ app.js
@@ -0,0 +1,14 @@
+//Book Class represents a book
+class Book {
+  constructor(title, author, isbn) {}
+}
+
+//UI Class to handle UI tasks
+
+//Store Class handles local storage
+
+//Event to display books
+
+//Event to add a book
+
+//Event to remove a book
