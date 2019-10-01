ea8145a1916b3afd90768e160e317e8164e75b6f
randyttphane <randyttphan@gmail.com>
2019-09-30 23:55:51 -0500

    cleared fields after adding a book

--- app.js
+++ app.js
@@ -42,6 +42,12 @@ class UI {
 
     list.appendChild(row);
   }
+
+  static clearFields() {
+    document.querySelector("#title").value = "";
+    document.querySelector("#author").value = "";
+    document.querySelector("#isbn").value = "";
+  }
 }
 
 //Store Class handles local storage
@@ -50,5 +56,23 @@ class UI {
 document.addEventListener("DOMContentLoaded", UI.displayBooks);
 
 //Event to add a book
+document.querySelector("#book-form").addEventListener("submit", e => {
+  //Preventing actual submit
+  e.preventDefault();
+
+  //Get form values
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const isbn = document.querySelector("#isbn").value;
+
+  //Instantiate book
+  const book = new Book(title, author, isbn);
+
+  //Add book to UI
+  UI.addBookToList(book);
+
+  //Clearing fields
+  UI.clearFields();
+});
 
 //Event to remove a book
